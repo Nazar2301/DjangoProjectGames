bc9329e407d2adbbef68b4e43a78691048e9b649
Konstantin <konstruktin@yandex.ru>
2025-01-29 18:17:58 +0300

    Add Buyer and Game models and create initial data

--- task1/views.py
+++ task1/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from .forms import UserRegister
+from .models import Buyer
+from .models import Game
 
-# Create your views here.
+users = ['existing_user1', 'existing_user2']
+
+# Главная страница
+def home(request):
+    return render(request, 'task1/home.html')
+
+# Список товаров
+def shop(request):
+    games = [
+        {'title': 'Cyberpunk 2007', 'description': 'Game of the year', 'cost': 31.00},
+        {'title': 'Mario', 'description': 'Old game', 'cost': 5.00},
+        {'title': 'Hitman', 'description': 'Who kills Mark?', 'cost': 12.00},
+    ]
+    context = {'games': games}
+    return render(request, 'task1/shop.html', context)
+
+# Корзина
+def cart(request):
+    return render(request, 'task1/cart.html')
+
+# Регистрация
+def sign_up_by_django(request):
+    info = {}
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = int(form.cleaned_data['age'])
+
+            # Получаем всех пользователей из таблицы Buyer
+            buyers = Buyer.objects.all()
+            buyer_names = [buyer.name for buyer in buyers]
+
+            if password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif username in buyer_names:
+                info['error'] = 'Пользователь уже существует'
+            else:
+                # Создаем нового пользователя
+                Buyer.objects.create(name=username, balance=0.00, age=age)
+                info['message'] = f'Приветствуем, {username}!'
+        info['form'] = form
+    else:
+        info['form'] = UserRegister()
+    return render(request, 'task1/registration_page.html', info)
